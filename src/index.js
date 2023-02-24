af26371aea63ab97e163cdc20871e643999572c8
Rodrigo Bossini <professorbossini@gmail.com>
2023-02-24 20:31:09 -0300

    apostila04(props): fim da apostila
    - componente Feedback criado e utilizado

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import Pedido from './Pedido'
 import Cartao from './Cartao'
+import Feedback from './Feedback'
 const App = () => {
     // const agora = new Date()
+    const textoOK = "Já chegou"
+    const textoNOK = "Ainda não chegou"
+    const funcaoOK = () => alert ("Agradecemos a confirmação!")
+    const funcaoNOK = () => alert ("Vamos verificar!")
+    const componenteFeedback = <Feedback textoOK={textoOK} textoNOK={textoNOK} funcaoOK={funcaoOK} funcaoNOK={funcaoNOK}/>
     return <div className="container border rounded mt-2">
         <div className="row">
             <h1 className="display-5 text-center">Seus pedidos</h1>
@@ -17,7 +23,8 @@ const App = () => {
                     <Pedido 
                         icone="fas fa-hdd fa-2x"
                         titulo="SSD"
-                        descricao="SSD Kingston A400 - Sata"/>    
+                        descricao="SSD Kingston A400 - Sata"/>
+                    {componenteFeedback}      
                 </Cartao>
             </div>
         </div>
@@ -29,6 +36,7 @@ const App = () => {
                         icone="fas fa-book fa-2x"
                         titulo="Livro"
                         descricao="Concrete Mathematics - Donald Knuth"/>  
+                    {componenteFeedback}
                 </Cartao>
             </div>
         </div>
@@ -40,6 +48,7 @@ const App = () => {
                         icone="fas fa-laptop fa-2x"
                         titulo="Notebook"
                         descricao="Notebook Dell - 8Gb - i5"/>
+                    {componenteFeedback}
                 </Cartao>
             </div>
         </div>
---
+++ teste_arrow_functions.js
@@ -0,0 +1,18 @@
+// const hello = () => { console.log ("oi")}
+// hello()
+
+// const hello = () => console.log("Oi")
+// hello()
+
+// const soma = (a, b) => { return a + b }
+// console.log(soma(2, 3))
+
+// const soma = (a, b) => a + b
+// console.log(soma(2, 3))
+
+const contaComplicada = (a, b, c) => {
+    console.log("oi")
+    return a * a + b * 2 + c * a + a * a + a * b
+}
+
+console.log(contaComplicada(1, 2, 3))
